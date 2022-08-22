0ec9e1f974fbd2dff36a5af925353b997daba0cd
3119004978 <1584132388@qq.com>
2022-08-22 11:19:05 +0800

    签名文件更新及查看更多功能

--- app/src/main/java/com/qxy/NoError/user/fragment/FollowListFragment.java
+++ app/src/main/java/com/qxy/NoError/user/fragment/FollowListFragment.java
@@ -1,6 +1,7 @@
 package com.qxy.NoError.user.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.qxy.NoError.databinding.FragmentListDetailBinding;
 import com.qxy.NoError.user.adapter.FollowingListAdapter;
@@ -20,6 +22,7 @@ public class FollowListFragment extends Fragment {
     private FragmentListDetailBinding binding;
     private UserViewModel userViewModel;
     private FollowingListAdapter followingListAdapter;
+    int count = 10;
 
     @Nullable
     @Override
@@ -40,6 +43,21 @@ public class FollowListFragment extends Fragment {
         userViewModel.getFollowingLiveData().observe(getViewLifecycleOwner(),(followList)->{
             followingListAdapter.setData(followList);
             binding.rvList.setAdapter(followingListAdapter);
+        });
+
+        //设置刷新触发事件
+        binding.srlList.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        count += 5;
+                        userViewModel.getFollowListData(0,count);
+                        binding.srlList.setRefreshing(false);
+                    }
+                }, 1000);
+            }
         });
     }
 }
